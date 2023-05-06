e26cd55ce18b67a4e10d01a79ee59d5c85dd7680
Sergio Forja Barriga <sergio.forja88@gmail.com>
2023-05-06 14:49:05 +0200

    Modificaciones en repo interno para añadir componente e interfaz (Ej3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleItemComponent } from './article-item/article-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/article-item/article-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article';
+
+@Component({
+  selector: 'app-article-item',
+  templateUrl: './article-item.component.html',
+  styleUrls: ['./article-item.component.css']
+})
+export class ArticleItemComponent implements Article {
+  name: string = 'Nike Court Vision Low';
+  imageUrl: string = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/818bb456-4fb5-49c9-bc6c-51d7f4d0f371/court-vision-low-zapatillas-1xL2Lc.png';
+  price: number = 45;
+  isOnSale: boolean = true;
+  quantityInCart: number = 0;
+
+  decrement() {
+    if (this.quantityInCart > 0) {
+      this.quantityInCart--;
+    }
+  }
+
+  increment() {
+    this.quantityInCart++;
+  }
+}
